5f0e97a891aea80f09c94a1e5392bfb5efc8fa6c
Joseph Choi <chousemath@gmail.com>
2016-06-06 21:15:35 +0900

    trying to get photo detection 4

--- server.py
+++ server.py
@@ -69,7 +69,7 @@ def webhook_handler():
         if text:
             text_array = text.split()
             handle_command(text_array[0], update)
-        elif not photo:
+        elif photo:
             filter_image(bot, update)
 
         # try:
@@ -115,44 +115,50 @@ def filter_image(bot, update):
     if not os.path.exists(chat_id):
         os.makedirs(chat_id)
     bot.getFile(file_id).download(chat_id+'/download.jpg')
-    img = Image.open(chat_id+'/download.jpg')
-    # No filter provided. Use a default filter.
-    reply = ', '.join(filters.keys())
-    if not update.message.caption:
-        msg_part_1 = 'Please provide the name of the filter you would like to '
-        msg_part_2 = 'use in the image\'s caption. Filters:\n\n'
-        reply = msg_part_1 + msg_part_2 + reply
 
-        # Notify the user of invalid input
-        bot.sendMessage(update.message.chat_id, text=reply)
+    bot.sendPhoto(update.message.chat_id,
+                  photo=open(chat_id+'/download.jpg', 'rb'),
+                  caption=('...and, here\'s your image inverted.'))
 
-        # Send sample filtered images in this scenario
-        img_greyscale = img.convert('L')
-        img_greyscale.save(chat_id+'/filtered.jpg')
-        bot.sendPhoto(update.message.chat_id,
-                      photo=open(chat_id+'/filtered.jpg', 'rb'),
-                      caption=('Meanwhile, here\'s your image in greyscale.'))
+    # img = Image.open(chat_id+'/download.jpg')
+    # # No filter provided. Use a default filter.
+    # reply = ', '.join(filters.keys())
+    # if not update.message.caption:
+    #     msg_part_1 = 'Please provide the name of the filter you would liketo'
+    #     msg_part_2 = 'use in the image\'s caption. Filters:\n\n'
+    #     reply = msg_part_1 + msg_part_2 + reply
+    #
+    #     # Notify the user of invalid input
+    #     bot.sendMessage(update.message.chat_id, text=reply)
+    #
+    #     # Send sample filtered images in this scenario
+    #     img_greyscale = img.convert('L')
+    #     img_greyscale.save(chat_id+'/filtered.jpg')
+    #     bot.sendPhoto(update.message.chat_id,
+    #                   photo=open(chat_id+'/filtered.jpg', 'rb'),
+    #                   caption=('Meanwhile, here\'s your image in greyscale.'))
+    #
+    #     # make sepia ramp (tweak color as necessary)
+    #     sepia = make_linear_ramp((255, 220, 192))
+    #     # optional: apply contrast enhancement here, e.g.
+    #     img_sepia = ImageOps.autocontrast(img_greyscale)
+    #     # apply sepia palette
+    #     img_sepia.putpalette(sepia)
+    #     # convert back to RGB so we can save it as JPEG
+    #     # (alternatively, save it in PNG or similar)
+    #     img_sepia = img_sepia.convert('RGB')
+    #     img_sepia.save(chat_id+'/sepia.jpg')
+    #     bot.sendPhoto(update.message.chat_id,
+    #                   photo=open(chat_id+'/sepia.jpg', 'rb'),
+    #                   caption=('...and, here\'s your image in sepia.'))
+    #     img_inv = ImageOps.invert(img)
+    #     img_inv.save(chat_id+'/inverted.jpg')
+    #     bot.sendPhoto(update.message.chat_id,
+    #                   photo=open(chat_id+'/inverted.jpg', 'rb'),
+    #                   caption=('...and, here\'s your image inverted.'))
 
-        # make sepia ramp (tweak color as necessary)
-        sepia = make_linear_ramp((255, 220, 192))
-        # optional: apply contrast enhancement here, e.g.
-        img_sepia = ImageOps.autocontrast(img_greyscale)
-        # apply sepia palette
-        img_sepia.putpalette(sepia)
-        # convert back to RGB so we can save it as JPEG
-        # (alternatively, save it in PNG or similar)
-        img_sepia = img_sepia.convert('RGB')
-        img_sepia.save(chat_id+'/sepia.jpg')
-        bot.sendPhoto(update.message.chat_id,
-                      photo=open(chat_id+'/sepia.jpg', 'rb'),
-                      caption=('...and, here\'s your image in sepia.'))
-        img_inv = ImageOps.invert(img)
-        img_inv.save(chat_id+'/inverted.jpg')
-        bot.sendPhoto(update.message.chat_id,
-                      photo=open(chat_id+'/inverted.jpg', 'rb'),
-                      caption=('...and, here\'s your image inverted.'))
-
-        return
+    #     return
+    return
 
 
 def echo(bot, update):
