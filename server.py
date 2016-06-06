bc823cd17a1039b99e7fe55cb883eede296f7c71
Joseph Choi <chousemath@gmail.com>
2016-06-06 20:45:28 +0900

    updated the heroku url

--- server.py
+++ server.py
@@ -75,7 +75,7 @@ def webhook_handler():
 
 # @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('https://telegram-filter-bot.herokuapp.com/HOOK')
+    s = bot.setWebhook('https://damp-castle-40734.herokuapp.com/HOOK')
     if s:
         return "webhook setup ok"
     else:
